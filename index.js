f5fd2ae1f30bbe594deb3d8fee9973ea717f87cb
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2025-05-02 13:05:26 +0100

    create append method

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 console.log("Console working!");
 
 class LinkedList {
-  
+  /* Adds new node to the end of the list */
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    } else {
+      let tmp = this.head;
+
+      while (tmp.next !== null) {
+        tmp = tmp.next;
+      }
+
+      tmp.next = new Node(value);
+    }
+  }
 }
 
 class Node {
@@ -12,5 +25,12 @@ class Node {
 }
 
 const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
 console.log(list);
